6d91e8e97043d2b97625eb9d8c8912d2d94f7381
dkothari-clgx <dkothari@corelogic.com>
2021-01-15 13:30:11 -0800

    Initial Commit

---
+++ src/main/java/io/djk/springkafkajdbcsink/SpringKafkaJdbcSinkApplication.java
@@ -0,0 +1,13 @@
+package io.djk.springkafkajdbcsink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaJdbcSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaJdbcSinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/djk/springkafkajdbcsink/SpringKafkaJdbcSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.djk.springkafkajdbcsink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaJdbcSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
